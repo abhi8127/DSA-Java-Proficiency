9c3e77c70b50bd78c52525f22566b1d6a23485f2
abhi8127 <abhisheky06880@gmail.com>
2024-12-30 23:24:49 +0530

    no of consonant

---
+++ MasaiSchool/module1213/CountConsonant.java
@@ -0,0 +1,15 @@
+package MasaiSchool.module1213;
+
+public class CountConsonant {
+    public static void main(String[] args) {
+        String str="abhishek";
+        int count=0;
+        for(int i=0;i<str.length();i++) {
+            if (str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u')
+            {
+            count++;
+            }
+        }
+        System.out.println(str.length()-count);
+    }
+}
